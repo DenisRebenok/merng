c5b8a2f5244e867dbcfc4cc638fb29254e9f0713
Denis Rebenok <denis.rebenok.pro@gmail.com>
2019-11-28 02:17:17 +0200

    Apollo Setup

---
+++ client/src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './App.css'
+
+export default () => {
+  return (
+    <div className="App">
+      <h1>Hi World</h1>
+    </div>
+  )
+}
---
+++ client/src/index.js
@@ -0,0 +1,6 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import ApolloProvider from './ApolloProvider'
+import App from './App'
+
+ReactDOM.render(<ApolloProvider />, document.getElementById('root'))
